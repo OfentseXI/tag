9a236833b2db2c86de9413589767b5287a5b96ac
Inolofatseng Motloba <156814403+OfentseXI@users.noreply.github.com>
2025-05-13 14:40:49 +0200

    Added John and Nico's content

--- app/page.js
+++ app/page.js
@@ -10,23 +10,35 @@ export default function Home() {
   
   const artists = [
     {
-      name: 'Monde',
-      role: 'Senior Artist',
-      image: '/assets/artists/monde.jpg',
-      bio: 'Our senior artist with over 4 years of experience, known for bold, intricate sleeves and large-scale masterpieces. Precision meets artistry in every piece.',
+      name: "Monde",
+      role: "Senior Artist",
+      image: "/assets/artists/monde.jpg",
+      bio: "Our senior artist with over 4 years of experience, known for bold, intricate sleeves and large-scale masterpieces. Precision meets artistry in every piece.",
     },
     {
-      name: 'Lucy',
-      role: 'Junior Artist',
-      image: '/assets/artists/lucy.jpg',
-      bio: 'A junior artist with a passion for detail, specializing in complex miniatures and custom designs. If it involves skulls, he’s all in.',
+      name: "Lucy",
+      role: "Junior Artist",
+      image: "/assets/artists/lucy.jpg",
+      bio: "A junior artist with a passion for detail, specializing in complex miniatures and custom designs. If it involves skulls, he’s all in.",
     },
     {
-      name: 'Fancy',
-      role: 'Apprentice',
-      image: '/assets/artists/fancy.jpg',
-      bio: 'A rising talent and dedicated apprentice, bringing fresh creativity to small designs with flair and style.',
+      name: "Fancy",
+      role: "Apprentice",
+      image: "/assets/artists/fancy.jpg",
+      bio: "A rising talent and dedicated apprentice, bringing fresh creativity to small designs with flair and style.",
     },
+    {
+      name: "John D",
+      role: "Management",
+      image: "/assets/artists/fancy.jpg",
+      bio: "A rising talent and dedicated apprentice, bringing fresh creativity to small designs with flair and style.",
+    },
+    {
+      name: "Fancy",
+      role: "Apprentice",
+      image: "/assets/artists/fancy.jpg",
+      bio: "A rising talent and dedicated apprentice, bringing fresh creativity to small designs with flair and style.",
+    }
   ];
 
   return (
